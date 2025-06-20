a66212d629b557125a5d54ef7c081e639abb2591
ak12033 <anush20303@gmail.com>
2025-06-20 10:15:54 +0530

    completed

--- server/config/db.js
+++ server/config/db.js
@@ -4,7 +4,7 @@ import mongoose from "mongoose";
 const connectDB = async () => {
     try {
         mongoose.connection.on('connected', () => console.log("Database Connected"));
-        await mongoose.connect(`${process.env.MONGO_URI}/Testing1`);
+        await mongoose.connect(`${process.env.MONGO_URI}/SummarizeX`);
     } catch (error) {
         console.log(error.message);   
     }
